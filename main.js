877dca41ef600356350ffeb1a9758be2807ed641
(lor)enzo <lorenzoyanga@gmail.com>
2021-06-13 20:49:19 +0800

    finished that quick start guide

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+
+      webPreferences: {
+          preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+
+
+    win.loadFile('index.html')
+}
+
+/* to make electron create the window we need. */
+app.whenReady().then(() => {
+    createWindow()
+
+    // for macOS; open a window if none are open.
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+    
+})
+
+/*
+Whereas Linux and Windows apps quit when they have
+no windows open, macOS apps generally continue running 
+even without any windows open, and activating the app 
+when no windows are available should open a new one.
+*/
+
+// darwin --> macOS.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
