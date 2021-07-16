363b1542fbdc8b53846dafbd6bed3834222fb829
(lor)enzo <lorenzoyanga@gmail.com>
2021-07-16 17:22:38 +0800

    final update before installing  electron-json-storage

--- src/apitest.js
+++ src/apitest.js
@@ -1,23 +1,27 @@
-
-function apiTest2()
-{
+document.getElementById("test").addEventListener("click", function(){
     var req = new XMLHttpRequest();
     req.open("GET", "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=9C2F8B579B777036AD7B17C6A5BEC8FA&steamid=76561198133758253");
+    req.responseType = 'json';
+   
+    req.onload = function(e) {
+        if (this.status == 200)
+        {
+            console.log('response', this.response);
+            document.getElementById("target").innerHTML = "TARGET\n" + JSON.stringify(this.response);
+        }
+    }
+    
     req.send();
-    document.getElementById("target").innerHTML = req.responseText;
-    console.log(req.responseText);
-}
-
-
-function alertTest()
-{
-    alert("this is alertTest()");
-}
-
-document.getElementById("test").addEventListener("click", function(){
-    apiTest2();
+    
+    // fetch(req.response)
+    //     .then(function(response){
+    //         document.getElementById("target").innerHTML = "TEST\n" + JSON.stringify(response);
+    //         console.log("log;\n")
+    //         //console.log(JSON.stringify(req.responseText,null, 2));
+    //         console.log(req.responseType);
+    //     });
 });
-document.getElementById("alerter").addEventListener("click", function()
-{
-    alertTest();
+
+document.getElementById("alerter").addEventListener("click", function(){
+    alert("this is alertTest()");
 });
