577cbe11eaa48087a8ac27a257fa3e792f5e8eb5
(lor)enzo <lorenzoyanga@gmail.com>
2021-12-12 18:08:52 +0800

    progress-push before upgrading laptop

--- src/gameeditor.js
+++ src/gameeditor.js
@@ -11,3 +11,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('appid').innerText = id;
 })
+
+if (typeof db === 'undefined') {
+    // the variable is undefined
+    const db = require('electron-db');
+}
+
+function EditorFillup(){
+    // load the savefile
+    
+    // 1. go through the list in template;
+    // for every tag, look for the corresponding tag and insert the data there. 
+
+    // 2. loop through every constants/consequents 
+    // set them to not be editable
+
+}
